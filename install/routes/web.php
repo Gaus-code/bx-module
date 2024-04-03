2aa60577a8a7b0a064b3c36efc00b8a0c0fe8a72
Yulya Gaus <yulya.yulya.010202@gmail.com>
2024-04-03 22:48:47 +0300

    Add client info layout

--- install/routes/web.php
+++ install/routes/web.php
@@ -11,6 +11,7 @@ return function (RoutingConfigurator $routes) {
 	$routes->get('/registration/', new PublicPageController('/local/modules/up.ukan/views/login.php'));
 	$routes->get('/task/{id}/', new PublicPageController('/local/modules/up.ukan/views/detail.php'));
 	$routes->get('/client/{user_id}/', new PublicPageController('/local/modules/up.ukan/views/client.php'));
+	$routes->get('/client/{user_id}/info/', new PublicPageController('/local/modules/up.ukan/views/client-info.php'));
 	$routes->get('/create/task/', new PublicPageController('/local/modules/up.ukan/views/task-create.php'));
 	$routes->get('/create/project/', new PublicPageController('/local/modules/up.ukan/views/project-create.php'));
 };
--- views/client-info.php
+++ views/client-info.php
@@ -97,12 +97,56 @@ $APPLICATION->SetTitle("UKAN - super service");
 		<article class="content__name">
 			<h2 class="content__tittle">Ваш профиль</h2>
 		</article>
-		<article class="content__main">
-			<?php $APPLICATION->IncludeComponent('up:task.list', '', [
-				'CLIENT_ID' => (int)request()->get('user_id'),
-				'TAG_ID' => (int)request()->get('tag_id'),
-			]);
-			?>
+		<article class="profileBio">
+			<ul class="account__editData">
+				<li class="editData__item">
+					<h2 class="editData__title">Имя</h2>
+					<div class="editData__form">
+						<img src="<?= SITE_TEMPLATE_PATH ?>/assets/images/user.svg" alt="edit name" class="editData__img">
+						<p class="editData__input">Тут имя вывести</p>
+						<button class="editData__btn" type="button" id="accountEditName">
+							<img src="<?= SITE_TEMPLATE_PATH ?>/assets/images/edit.svg" alt="make your edits"
+								 class="editData__btnImg">
+						</button>
+					</div>
+				</li>
+				<li class="editData__item">
+					<h2 class="editData__title">Почта</h2>
+					<div class="editData__form">
+						<img src="<?= SITE_TEMPLATE_PATH ?>/assets/images/email.svg" alt="edit e-mail address"
+							 class="editData__img">
+						<p class="editData__input">Тут почту вывести</p>
+						<button class="editData__btn" type="button" id="accountEditEmail">
+							<img src="<?= SITE_TEMPLATE_PATH ?>/assets/images/edit.svg" alt="make your edits"
+								 class="editData__btnImg">
+						</button>
+					</div>
+				</li>
+				<li class="editData__item">
+					<h2 class="editData__title">Фамилия</h2>
+					<div class="editData__form">
+						<img src="<?= SITE_TEMPLATE_PATH ?>/assets/images/user.svg" alt="edit surname"
+							 class="editData__img">
+						<p class="editData__input">Тут фамилию вывести</p>
+						<button class="editData__btn" type="button" id="accountEditSurname">
+							<img src="<?= SITE_TEMPLATE_PATH ?>/assets/images/edit.svg" alt="make your edits"
+								 class="editData__btnImg">
+						</button>
+					</div>
+				</li>
+				<li class="editData__item">
+					<h2 class="editData__title">Пароль</h2>
+					<div class="editData__form">
+						<img src="<?= SITE_TEMPLATE_PATH ?>/assets/images/key.svg" alt="edit Password"
+							 class="editData__img">
+						<p class="editData__input">password</p>
+						<button class="editData__btn" type="button" id="accountEditPassword">
+							<img src="<?= SITE_TEMPLATE_PATH ?>/assets/images/edit.svg" alt="make your edits"
+								 class="editData__btnImg">
+						</button>
+					</div>
+				</li>
+			</ul>
 		</article>
 	</section>
 </main>
