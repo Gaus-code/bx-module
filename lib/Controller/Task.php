8217ef2d85d069ca6008cf6cdd2f4ba5149d329a
Nikita Tikhomirov <nikita.t.18.09@gmail.com>
2024-04-03 19:51:00 +0200

    change in task/createAction

--- lib/Controller/Task.php
+++ lib/Controller/Task.php
@@ -8,17 +8,16 @@ use Up\Ukan\Model\TaskTable;
 
 class Task extends Controller
 {
-	public function createAction(string $title, string $description = "", int $maxPrice = null, array $tags = [])
+	public function createAction(string $title, string $description, int $maxPrice = null, array $tags = [])
 	{
 		if (check_bitrix_sessid())
 		{
 			$task = new EO_Task();
-			$task->setTitle($title)->setDescription($description)->setMaxPrice($maxPrice);
-			$task->setClientId(1);
+			$task->setTitle($title)->setMaxPrice($maxPrice)->setDescription($description)->setClientId(1);
+
 			$task->save();
 
-
-			return $task;
+			LocalRedirect("/client/");
 		}
 	}
 }
