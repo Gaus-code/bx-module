26a2de31c568c5523f71c3b0a0216f17282ef301
Yulya Gaus <yulya.yulya.010202@gmail.com>
2024-04-09 18:51:15 +0300

    add project-edit layout

--- views/user-project.php
+++ views/user-project.php
@@ -5,13 +5,18 @@
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 $APPLICATION->SetTitle("UKAN - super service");
 ?>
+
 <?php
 
 if (!$USER->IsAuthorized())
 {
 	LocalRedirect('/sign-in');
 }
-var_dump('hello from user 1 project');
+?>
+
+<?php $APPLICATION->IncludeComponent('up:user.project', '', [
+	'USER_ID' => (int)$USER->GetID()
+]);
 ?>
 
 <?php require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
