5ae8197c0d1fa84a4b2a42cdff9db330b3cdffc5
Nikita Tikhomirov <nikita.t.18.09@gmail.com>
2024-04-17 01:26:05 +0200

    feedback

--- config.php
+++ config.php
@@ -8,9 +8,8 @@ return array(
 		'at_work' => 'В работе',
 	),
 	'subscription' => array(
-		'title' => 'Премиум',
-		'description' => '',
 		'price' => 399,
+		'trial_subscription_period_in_days'=>'30',
 	),
 	'yandexGPT' => array(
 		'secret_key' => 'AQVNztg5AhOJG_qdivT_LWgRQADPQkfjCwyY2tbP',
