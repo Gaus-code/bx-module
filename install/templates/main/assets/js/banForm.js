7a6682cfb370aab24d2af73dc4acd3dbb8cca96a
Yulya Gaus <yulya.yulya.010202@gmail.com>
2024-04-24 23:25:24 +0300

    add ban-btn to view profile page

--- install/templates/main/assets/js/banForm.js
+++ install/templates/main/assets/js/banForm.js
@@ -2,10 +2,7 @@ const banBtn = document.querySelector('.banBtn');
 const banForm = document.querySelector('.banForm');
 const closeFormBtn = document.querySelector('#closeFormBtn');
 
-
-// Add a click event listener to the button
 banBtn.addEventListener('click', () => {
-	// Show the form and hide the button with a fade-in/fade-out effect
 	banForm.style.transition = 'opacity 0.3s ease-in-out';
 	banForm.style.opacity = '1';
 	banBtn.style.opacity = '0';
@@ -13,9 +10,7 @@ banBtn.addEventListener('click', () => {
 	banForm.style.display = 'block';
 });
 
-// Add a click event listener to the close form button
 closeFormBtn.addEventListener('click', () => {
-	// Hide the form and show the button with a fade-in/fade-out effect
 	banForm.style.transition = 'opacity 0.3s ease-in-out';
 	banForm.style.opacity = '0';
 	banBtn.style.opacity = '1';
